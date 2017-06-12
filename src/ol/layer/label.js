a88aec83a6c20a3adb4a09260641367c4c5846a8
AndreasP <a15237@freenet.de>
2017-06-12 22:51:10 +0200

    Added test implementation of ol.style.Label

--- src/ol/layer/label.js
+++ src/ol/layer/label.js
@@ -1,5 +1,15 @@
 ol.layer.Label = function(opt_options) {
 
+  if(!opt_options.style) {
+    opt_options.style = new ol.style.Style({
+    	text: new ol.style.Label({
+    		label_factor: 10,
+        	name: 'Teststadt',
+        	font_string: '',
+        })
+    })
+  }
+
   ol.layer.Vector.call(this, opt_options);
 
 };
