b0c4984e2e0803cccdd738115c738d62a29d65ca
AndreasP <a15237@freenet.de>
2017-07-31 15:01:52 +0200

    Made drawCircleCheckboxDummy visible

--- src/ol/control/label-debug.js
+++ src/ol/control/label-debug.js
@@ -1,11 +1,10 @@
 ol.control.LabelDebug = function(opt_options) {
 
-  console.log("Initialising labeldebug");
-
   var options = opt_options ? opt_options : {};
 
   var className = options.className !== undefined ? options.className : 'ol-label-debug';
 
+  // Checkbox for enabling the drawing of the circles
   var drawCirclesCheckbox = document.createElement('input');
   drawCirclesCheckbox.setAttribute('type', 'checkbox');
   drawCirclesCheckbox.id = "drawCirclesCheckbox";
@@ -17,29 +16,25 @@ ol.control.LabelDebug = function(opt_options) {
   ol.events.listen(drawCirclesCheckbox, ol.events.EventType.CHANGE, 
     ol.control.LabelDebug.prototype.drawCircles_.bind(this));
 
+  // the parent div
   var cssClasses = className + ' ' + ol.css.CLASS_UNSELECTABLE + ' ' +
       ol.css.CLASS_CONTROL;
   var element = document.createElement('div');
   element.className = cssClasses;
   element.appendChild(drawCirclesCheckbox);
   element.appendChild(drawCircleLabel);
+  Object.assign(element.style,{background:"green", bottom:"65px"});
 
   ol.control.Control.call(this, {
     element: element,
     target: options.target
-  });
-
-  /**
-   * @type {number}
-   * @private
-   */
-  this.duration_ = options.duration !== undefined ? options.duration : 250;
+  });  
 
 };
 ol.inherits(ol.control.LabelDebug, ol.control.Control);
 
 ol.control.LabelDebug.prototype.drawCircles_ = function(event) {
   event.preventDefault();
-  console.log("drawCirclesCheckbox, state changed.");
+  // TODO: enable/disable circles
 };
 
